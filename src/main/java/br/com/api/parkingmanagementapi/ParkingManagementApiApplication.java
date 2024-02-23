f0e4dfe20f04f864fce1af49f86ee1523c4dc4f8
Lucas Soares Maciel <lucas.tibetano@gmail.com>
2024-02-23 14:15:52 -0300

    Created the initial basic structure of the project.

---
+++ src/main/java/br/com/api/parkingmanagementapi/ParkingManagementApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.parkingmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api/parkingmanagementapi/ParkingManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.parkingmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
